421b308bad43fc8510d71bdc796c927ca89f5c1a
FARINE KHAN <farinepkhan520@gmail.com>
2023-04-27 11:35:09 +0530

    padding

--- porfolio/src/components/Porject.jsx
+++ porfolio/src/components/Porject.jsx
@@ -29,12 +29,12 @@ const Porject = () => {
   return (
     <div
       name="projects"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen sm:py-5"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Portfolio
+            Project
           </p>
           <p className="py-6">Check out some of my work right here</p>
         </div>
